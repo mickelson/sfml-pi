79166e4532f6fa85538b9b5f3e665605182e3d9f
fiskn <nick@fisk.me.uk>
2020-10-01 21:45:41 +0100

    Add standard X & Y events for EV_ABS
    Touchscreen emulates a mouse and sends ABS_X & ABS_Y

--- src/SFML/Window/Unix/DRM/InputImplUDev.cpp
+++ src/SFML/Window/Unix/DRM/InputImplUDev.cpp
@@ -469,8 +469,18 @@ namespace
                 }
                 else if ( ie.type == EV_ABS )
                 {
+                    bool posChange = false;
                     switch ( ie.code )
                     {
+                    case ABS_X:
+                        mousePos.x = ie.value;
+                        posChange = true;
+                        break;
+
+                    case ABS_Y:
+                        mousePos.y = ie.value;
+                        posChange = true;
+                        break;
                     case ABS_MT_SLOT:
                         currentSlot = ie.value;
                         touchFd = *itr;
@@ -487,6 +497,13 @@ namespace
                         atSlot(currentSlot).pos.y = ie.value;
                         touchFd = *itr;
                         break;
+                    }
+                    if ( posChange )
+                    {
+                        ev.type = sf::Event::MouseMoved;
+                        ev.mouseMove.x = mousePos.x;
+                        ev.mouseMove.y = mousePos.y;
+                        return true;
                     }
                 }
                 else if ( ie.type == EV_SYN && ie.code == SYN_REPORT &&
